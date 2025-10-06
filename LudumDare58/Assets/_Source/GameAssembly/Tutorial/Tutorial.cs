152ae0ee746324eb6937fde3f229ee49a64eb754
Алексей <ix.fenix@mail.ru>
2025-10-07 00:21:09 +0300

    feat: tutorial

---
+++ LudumDare58/Assets/_Source/GameAssembly/Tutorial/Tutorial.cs
@@ -0,0 +1,30 @@
+﻿using PlayerSystem;
+using UnityEngine;
+using VContainer;
+
+namespace Tutorial
+{
+    public class Tutorial : MonoBehaviour //TODO: Disable tutorial show every level load
+    {
+        [SerializeField] private GameObject tutorialObject;
+        
+        [Inject] private InputSystem_Actions _input;
+
+        private void Start()
+        {
+            ActivateTutorial();
+        }
+
+        private void ActivateTutorial()
+        {
+            _input.Player.Disable();
+            tutorialObject.gameObject.SetActive(true);
+        }
+
+        public void DeactivateTutorial()
+        {
+            _input.Player.Enable();
+            tutorialObject.gameObject.SetActive(false);
+        }
+    }
+}
